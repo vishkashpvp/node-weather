ce32f0c69b929d024d62cda4e64dc307f0023837
Vishkash <vishkash.vishnu@gmail.com>
2021-11-17 15:53:15 +0530

    Edited forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find location. Try another search', undefined)
         }
         else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. There is a ' + body.current.precip + '% chance of rain')
+            callback(undefined, body.current.weather_descriptions[0] + ', time is ' + body.current.observation_time + '. It is currently ' + body.current.temperature + ' degrees out. There is a ' + body.current.precip + '% chance of rain')
         }
     })
 }
